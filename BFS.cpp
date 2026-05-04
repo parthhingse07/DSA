f2c6fbbaca5fbd20cd885467f6d27e00e71e545e
parthhingse07 <parthhingse07@gmail.com>
2026-05-04 23:34:09 +0530

    Create BFS.cpp

---
+++ BFS.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+void BFS(int start, vector<vector<int>> &adj, int V) {
+    vector<bool> visited(V, false);
+    queue<int> q;
+
+    visited[start] = true;
+    q.push(start);
+
+    cout << "BFS Traversal: ";
+
+    while (!q.empty()) {
+        int node = q.front();
+        q.pop();
+
+        cout << node << " ";
+
+        for (int neighbor : adj[node]) {
+            if (!visited[neighbor]) {
+                visited[neighbor] = true;
+                q.push(neighbor);
+            }
+        }
+    }
+}
+
+int main() {
+    int V = 5;
+
+    vector<vector<int>> adj(V);
+
+    // Adding edges
+    adj[0] = {1, 2};
+    adj[1] = {0, 3, 4};
+    adj[2] = {0};
+    adj[3] = {1};
+    adj[4] = {1};
+
+    BFS(0, adj, V);
+
+    return 0;
+}
